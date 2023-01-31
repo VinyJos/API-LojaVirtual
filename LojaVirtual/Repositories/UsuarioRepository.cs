4412898fa8fae0e4cc8ba9d36769bec9acdb2328
Vinicius Jose <viniciuswk@gmail.com>
2023-01-31 03:00:24 -0300

    sql connection

--- LojaVirtual/Repositories/UsuarioRepository.cs
+++ LojaVirtual/Repositories/UsuarioRepository.cs
@@ -1,6 +1,54 @@
-﻿namespace LojaVirtual.Repositories
+﻿using LojaVirtual.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LojaVirtual.Repositories
 {
-    public class UsuarioRepository
+    public class UsuarioRepository : IUsuarioRepository
     {
+        private IDbConnection _connection;
+
+        public UsuarioRepository()
+        {
+            SqlConnection
+        }
+
+        public void CriarUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditarVerificacaoUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Categoria> ListagemDeCategorias()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Produto> ProdutosPorCategoria(string url)
+        {
+            throw new NotImplementedException();
+        }
+        public Produto GetProduto(string url)
+        {
+            throw new NotImplementedException();
+        }
+        public void AutenticaUsuario()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NovoPedido(Pedido pedido)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Pedido> ListaPedidos()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
